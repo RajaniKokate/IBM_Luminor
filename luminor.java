2156df633e9ffc76dfaeaa60c5c2b6ba17ae5d28
RajaniKokate <39612817+RajaniKokate@users.noreply.github.com>
2021-04-15 14:23:00 +0530

    Update luminor.java

--- luminor.java
+++ luminor.java
@@ -1,13 +1,26 @@
-package ibm1;
-import static com.codeborne.selenide.Selenide.*;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 
-public class luminor {
-	
-	
-open("https://luminor.ee/currency-rates");
+public class luminor{
+
+    @Test
+    public void simple() throws InterruptedException {
+        open("https://luminor.ee/currency-rates");
+        Thread.sleep(25000);
+        $(byXpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/div[3]/div/div/div[1]/div/div[1]/div/div[1]/div/input")).setValue("100");
+        Thread.sleep(2000);
+        String a = $(byXpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div[1]/div/input")).getAttribute("value");
+        System.out.println("Value is:"+ a);
+    }
+    }
 
 
-		
 
-}
+
+
+
+
